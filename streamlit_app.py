e9f0d3452522a39f7d505bf2037b77308ed3f9f5
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 13:23:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,21 +8,6 @@ st.title("Test App")
 # audio_file = open("door_knock.mp3","rb").read()
 # st.audio(audio_file,format='auido/mp3')
 
-import base64
-# import streamlit as st
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio controls autoplay="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
 
 
 st.button(label="Start", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
