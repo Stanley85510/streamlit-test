1cfc31e92378266a09b310fa7966035fca79fb20
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 00:09:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit as st
 st.title("Test App")
 
 # playing door knock
-import audioplayer
-audioplayer.play_mp3("door_knock.mp3")
+# import audioplayer
+# audioplayer.play_mp3("door_knock.mp3")
+
+import pygame
+pygame.mixer.init()
