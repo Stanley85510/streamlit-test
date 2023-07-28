f12526b554f8fe8000bab9b364aa6ab80fde160c
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 13:27:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.title("Test App")
 
 
-st.button(label="Start", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+st.button(label="Start", key=None, help=None, on_click=import be_conversation, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 
 
