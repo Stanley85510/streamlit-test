2cac0b01fcaede32a8fde15f66bb8f1e9c0143b1
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 13:26:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,7 @@
 import streamlit as st
 st.title("Test App")
 
-# playing door knock
-# import audioplayer
-# audioplayer.play_mp3("door_knock.mp3")
-
-# audio_file = open("door_knock.mp3","rb").read()
-# st.audio(audio_file,format='auido/mp3')
-
-
 
 st.button(label="Start", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 
-autoplay_audio("door_knock.mp3")
-autoplay_audio("door_greeting.mp3")
+
