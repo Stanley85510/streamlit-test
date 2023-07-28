eb991b6deb14800d85d7a924045b8741b8b92d2a
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-27 18:06:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("Sales Role Play")
 import modules
 modules.install_pygame()
 
+# Starting the simulation with a door knock and the user answering the door
+import audioplayer
+audioplayer.play_mp3("door_knock.mp3")
