917dac1c7f729404191358f06bf0c2bff489b8a7
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 00:14:05 -0700

    simplified code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 st.title("Test App")
 
-# playing door knock
-# import audioplayer
-# audioplayer.play_mp3("door_knock.mp3")
+playing door knock
+import audioplayer
+audioplayer.play_mp3("door_knock.mp3")
 
-import subprocess 
-subprocess.run(["pip", "install", "pygame"])
-
-import pygame
-pygame.mixer.init()
