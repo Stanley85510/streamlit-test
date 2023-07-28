2a3a29d8e573ab4d6b62bf955fe7b9d9f31d2f87
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-27 18:01:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,16 @@ import streamlit as st
 st.title("Sales Role Play")
 
 import subprocess
-
 def install_pygame():
     try:
-        subprocess.run(["pip", "install", "pygame"], check=True)
-        print("pygame installed successfully.")
+        result = subprocess.run(["pip", "install", "pygame"], check=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            print("pygame installed successfully.")
+        else:
+            print("pygame installation failed.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     install_pygame()
+
