1b12142eb837eb3997bc3eccbcf560eaa02e9a2a
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 14:06:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,28 @@
+#App Front End
 import streamlit as st
 st.title("Test App")
 st.write("Click the button to Start.")
 
+#Define functions
+import base64
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+#App Backend
 if st.button("Start"):
     st.write("simulation started")
-
+    autoplay_audio("door_knock.mp3")
+    autoplay_audio("door_greeting.mp3")
 
 
