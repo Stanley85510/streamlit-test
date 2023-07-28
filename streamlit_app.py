98999aa4e39375fcbca386d7535a56534aa6eae7
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 14:03:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit as st
 st.title("Test App")
 st.write("Click the button to Start.")
 
-import call_script
-
 if st.button("Start"):
-    call_script.call_another_script("be_conversation.py")
     st.write("simulation started")
 
 
