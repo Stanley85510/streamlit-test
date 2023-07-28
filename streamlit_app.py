dcbb1098c3186fab7b5ee5407105774e975e3917
Stanley85510 <132618344+Stanley85510@users.noreply.github.com>
2023-07-28 12:52:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 st.title("Test App")
 
-playing door knock
-import audioplayer
-audioplayer.play_mp3("door_knock.mp3")
+# playing door knock
+# import audioplayer
+# audioplayer.play_mp3("door_knock.mp3")
+
+audio_file = open.("door_knock.mp3","rb").open()
+st.audio(auido_file,format="auido/mp3")
 
